27455322fb83c5ab202d90114883a40c7bfb55c6
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2024-12-15 12:08:50 +0530

    Basic Changes

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -2,7 +2,7 @@ require('neoscroll').setup({
   mappings = {                 -- Keys to be mapped to their corresponding default scrolling animation
     '<C-u>', '<C-d>',
     '<C-b>', '<C-f>',
-    '<C-y>', '<C-e>',
+    '<C-y>', '<C-u>',
     'zt', 'zz', 'zb',
   },
   hide_cursor = true,          -- Hide cursor while scrolling
