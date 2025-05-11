b72443266020224421a6e97884d15b903d43fd0e
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-05-11 18:12:11 +0530

    feat: add key mappings for telescope and update plugin configuration

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,6 +7,7 @@ vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {
 vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", {
     desc = "Toggle file explorer"
 })
+
 vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", {
     desc = "Toggle file explorer on current file"
 })
@@ -23,9 +24,3 @@ vim.keymap.set({'n', 'v'}, '<leader>y', '"*y', {
 vim.keymap.set({'n', 'v'}, '<leader>Y', '"+y', {
     desc = 'Copy to system clipboard (alternative)'
 })
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
